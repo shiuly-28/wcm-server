3f204b01867b6a48a90a73ea1fd8d73df11bd1d1
farhandotio <farhansadik0760@gmail.com>
2026-02-21 13:20:38 +0600

    cookie resolve

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -47,8 +47,8 @@ export const loginUser = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -8,8 +8,8 @@ export const sendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   };
 
   user.password = undefined;
